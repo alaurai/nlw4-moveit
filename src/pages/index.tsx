c4c7de9ef6ea8c4f78787dceac1b263b26f1bf3b
Anna Laura <alauraivani@gmail.com>
2021-02-26 11:19:16 -0300

    feat: add title and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,16 @@ import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { CountDown } from "../components/CountDown";
 
+import Head from 'next/head';
+
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
