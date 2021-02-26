431a55c9dbc2cae8fd2241549c2aadd77efd5ac7
Anna Laura <alauraivani@gmail.com>
2021-02-26 10:24:05 -0300

    feat: add ExperienceBar.module.css for ExperienceBar component

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,10 +1,13 @@
+import styles from '../styles/components/ExperienceBar.module.css'
+
+
 export function ExperienceBar() {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
                 <div style={{ width: '50%' }} />
-                <span className="currrent-experience" style={{ left: '50%' }}> 300px</span>
+                <span className={styles.currrentExperience} style={{ left: '50%' }}> 300px</span>
             </div>
             <span>600px</span>
         </header>
