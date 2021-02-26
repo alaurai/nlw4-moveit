aa5843ff10abee52d973567d0f5c608464a42d03
Anna Laura <alauraivani@gmail.com>
2021-02-26 10:45:01 -0300

    feat: add styles Home.module.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div></div>
+      </section>
     </div>
   )
 }
