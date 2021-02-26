493befe139d7a1bc2131dcc97adbf7217c1efa20
Anna Laura <alauraivani@gmail.com>
2021-02-26 13:48:30 -0300

    feat: add a button to countDown component

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -2,16 +2,22 @@ import styles from '../styles/components/CountDown.module.css'
 
 export function CountDown() {
     return (
-        <div className={styles.countDownContainer}>
-            <div>
-                <span>2</span>
-                <span>5</span>
+        <div>
+            <div className={styles.countDownContainer}>
+                <div>
+                    <span>2</span>
+                    <span>5</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>0</span>
+                    <span>0</span>
+                </div>
             </div>
-            <span>:</span>
-            <div>
-                <span>0</span>
-                <span>0</span>
-            </div>
+
+            <button type="button" className={styles.countDownButton}>
+                Iniciar um ciclo
+            </button>
         </div>
     )
 }
