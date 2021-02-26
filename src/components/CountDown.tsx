41bb64a153414a562b1d90fc7cfedfa7cc4a9224
Anna Laura <alauraivani@gmail.com>
2021-02-26 13:56:13 -0300

    feat: add state to countDown

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,17 +1,27 @@
+import { useState } from 'react'
 import styles from '../styles/components/CountDown.module.css'
 
 export function CountDown() {
+
+    const [time, setTime] = useState(25 * 60);
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
     return (
         <div>
             <div className={styles.countDownContainer}>
                 <div>
-                    <span>2</span>
-                    <span>5</span>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
                 </div>
                 <span>:</span>
                 <div>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
                 </div>
             </div>
 
