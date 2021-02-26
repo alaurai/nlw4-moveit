84c2bd496030ffcf1be4672b33b249bf7e5aa331
Anna Laura <alauraivani@gmail.com>
2021-02-26 10:03:27 -0300

    feat: add ExperienceBar component and global styles

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,12 @@
+export function ExperienceBar() {
+    return (
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{ width: '50%' }} />
+                <span className="currrent-experience" style={{ left: '50%' }}> 300px</span>
+            </div>
+            <span>600px</span>
+        </header>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ExperienceBar } from "../components/ExperienceBar";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  )
+}
